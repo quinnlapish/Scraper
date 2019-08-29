42f3d170e54826bd3bc3135f3e509da186b94ba3
Quinn Lapish <quinn.lapish99@gmail.com>
2019-08-29 14:39:35 -0400

    second

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connect("mongodb://localhost27017/scraperdatabase", { useNewUrlParser: 
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with axios
-  axios.get("https://www.nytimes.com").then(function(response) {
+  axios.get("https://nationalpost.com/category/news").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
